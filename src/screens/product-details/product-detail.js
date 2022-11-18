b817c50554f51b7da5b1185b040b6f58069aac02
HoCongViet2501 <viet.forwork.2501@gmail.com>
2022-11-18 10:19:19 +0700

    update product detail

--- src/screens/product-details/product-detail.js
+++ src/screens/product-details/product-detail.js
@@ -1,25 +1,34 @@
-import { Button } from '@react-native-material/core';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import FooterProduct from '../layout/footer';
-import HeaderProduct from '../layout/header';
+import { Ionicons } from '@expo/vector-icons';
+import { useEffect } from 'react';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import { FooterProduct } from '../layout/footer';
+import { HeaderProduct } from '../layout/header';
 
 export default function ProductDetail({ navigation: { navigate, goBack }, route }) {
     const { item } = route.params;
+    useEffect(() => {
+        console.log(item);
+    }, []);
     return (
         <View style={styles.container}>
             <HeaderProduct goBack={goBack}></HeaderProduct>
             <View style={styles.content}>
-                <View style={{ marginVertical: 15 }}>
-                    <Image style={styles.imageThumnail} source={item.url} />
-                </View>
-                <Text style={styles.productNameTxt}>{item.name}</Text>
-                <Text style={styles.priceTxt}>{item.price} VND</Text>
-                <Text style={styles.titleDescription}>Mô tả sản phẩm</Text>
-                <Text style={styles.contentDescription}>
-                    Trang trí ngôi nhà bạn trở nên hiện đại, nâng chất lượng cuộc sống của bạn lên một tầm cao mới sang trọng và quý's tộc hơn.
-                    Cảm biến chuyển động siêu nhạy tự động bật tắt đèn cầu thang khi có người đi lại..
-                </Text>
+                <ScrollView>
+
+                    <View style={{ marginVertical: 15 }}>
+                        <Image style={styles.imageThumnail} source={item.url} />
+                    </View>
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+                        <Text style={styles.productNameTxt}>{item.name}</Text>
+                        <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+                            <Ionicons name="md-star-sharp" size={24} color="#E8C03A" />
+                            <Text style={{ fontWeight: '700' }}>4.5<Text style={{ fontWeight: 'normal' }}> (10)</Text></Text>
+                        </View>
+                    </View>
+                    <Text style={styles.priceTxt}>${item.price}</Text>
+                    <Text style={styles.titleDescription}>Description</Text>
+                    <Text style={styles.contentDescription}>{item.description}</Text>
+                </ScrollView>
             </View>
             <FooterProduct></FooterProduct>
         </View>
@@ -30,19 +39,23 @@ const styles = StyleSheet.create({
     container: {
         position: 'relative',
         height: '100%',
+        backgroundColor: '#fff',
     },
     content: {
         width: '95%',
+        height: '78%',
         marginHorizontal: '2.5%',
     },
     imageThumnail: {
-        width: '100%',
-        height: 350
+        width: '90%',
+        height: 350,
+        marginLeft: '5%',
     },
     productNameTxt: {
         marginTop: 15,
         fontSize: 25,
         fontWeight: 'bold',
+        width: '70%'
     },
     priceTxt: {
         fontSize: 20,
