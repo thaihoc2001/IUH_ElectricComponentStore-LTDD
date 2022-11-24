1b4e5dfdc98c768d6781ee52cdff17ac61c491a9
thaihoc2001 <thaihoc95x@gmail.com>
2022-11-24 11:17:34 +0700

    update data

--- src/fake-data/store.js
+++ src/fake-data/store.js
@@ -5,6 +5,7 @@ export const LIST_PRODUCT = [
         price: 500,
         url: require('../../assets/images/iphone-11-trang.jpg'),
         isFavorite: true,
+        categoryId: 1,
         description: 'Trong bộ 3 iPhone 11 Series, iPhone 11 64GB tuy là phiên bản thường nhưng được hội tụ nhiều yếu tố hấp dẫn nhất, với thiết kế trẻ trung, sở hữu nhiều phiên bản màu nhất, kích thước vừa phải, không quá to và không quá nhỏ. Và còn một điều nữa, mà bạn sẽ bất ngờ, hãy cùng Điện thoại Giá Kho tìm hiểu ngay!'
     },
     {
@@ -13,6 +14,7 @@ export const LIST_PRODUCT = [
         price: 800,
         url: require('../../assets/images/Galaxy-S22-Ultra-Burgundy.jpg'),
         isFavorite: false,
+        categoryId: 1,
         description: 'Trong bộ 3 iPhone 11 Series, iPhone 11 64GB tuy là phiên bản thường nhưng được hội tụ nhiều yếu tố hấp dẫn nhất, với thiết kế trẻ trung, sở hữu nhiều phiên bản màu nhất, kích thước vừa phải, không quá to và không quá nhỏ. Và còn một điều nữa, mà bạn sẽ bất ngờ, hãy cùng Điện thoại Giá Kho tìm hiểu ngay!'
     },
     {
@@ -21,6 +23,7 @@ export const LIST_PRODUCT = [
         price: 1000,
         url: require('../../assets/images/samsung-galaxy-z-flip4.jpg'),
         isFavorite: true,
+        categoryId: 1,
         description: 'Trong bộ 3 iPhone 11 Series, iPhone 11 64GB tuy là phiên bản thường nhưng được hội tụ nhiều yếu tố hấp dẫn nhất, với thiết kế trẻ trung, sở hữu nhiều phiên bản màu nhất, kích thước vừa phải, không quá to và không quá nhỏ. Và còn một điều nữa, mà bạn sẽ bất ngờ, hãy cùng Điện thoại Giá Kho tìm hiểu ngay!'
     },
     {
@@ -29,6 +32,7 @@ export const LIST_PRODUCT = [
         price: 400,
         url: require('../../assets/images/iphone-11-trang.jpg'),
         isFavorite: false,
+        categoryId: 1,
         description: 'Trong bộ 3 iPhone 11 Series, iPhone 11 64GB tuy là phiên bản thường nhưng được hội tụ nhiều yếu tố hấp dẫn nhất, với thiết kế trẻ trung, sở hữu nhiều phiên bản màu nhất, kích thước vừa phải, không quá to và không quá nhỏ. Và còn một điều nữa, mà bạn sẽ bất ngờ, hãy cùng Điện thoại Giá Kho tìm hiểu ngay!'
     },
     {
@@ -36,9 +40,56 @@ export const LIST_PRODUCT = [
         name: 'Mach dieu khien led cau thang thong minh',
         price: 1010,
         url: require('../../assets/images/iphone-11-trang.jpg'),
+        isFavorite: false,
+        categoryId: 1,
+        description: 'Trong bộ 3 iPhone 11 Series, iPhone 11 64GB tuy là phiên bản thường nhưng được hội tụ nhiều yếu tố hấp dẫn nhất, với thiết kế trẻ trung, sở hữu nhiều phiên bản màu nhất, kích thước vừa phải, không quá to và không quá nhỏ. Và còn một điều nữa, mà bạn sẽ bất ngờ, hãy cùng Điện thoại Giá Kho tìm hiểu ngay!'
+    },
+    {
+        id: 6,
+        name: 'Asus TUF gaming FX506lhb',
+        price: 1010,
+        url: require('../../assets/images/asus-tuf-gaming-fx506lhb-i5.jpg'),
+        isFavorite: false,
+        categoryId: 2,
+        description: 'Trong bộ 3 iPhone 11 Series, iPhone 11 64GB tuy là phiên bản thường nhưng được hội tụ nhiều yếu tố hấp dẫn nhất, với thiết kế trẻ trung, sở hữu nhiều phiên bản màu nhất, kích thước vừa phải, không quá to và không quá nhỏ. Và còn một điều nữa, mà bạn sẽ bất ngờ, hãy cùng Điện thoại Giá Kho tìm hiểu ngay!'
+    },
+    {
+        id: 7,
+        name: 'Lenovo legion 5 15iah7',
+        price: 1200,
+        url: require('../../assets/images/lenovo-legion-5-15iah7-i5.jpg'),
         isFavorite: true,
+        categoryId: 2,
+        description: 'Trong bộ 3 iPhone 11 Series, iPhone 11 64GB tuy là phiên bản thường nhưng được hội tụ nhiều yếu tố hấp dẫn nhất, với thiết kế trẻ trung, sở hữu nhiều phiên bản màu nhất, kích thước vừa phải, không quá to và không quá nhỏ. Và còn một điều nữa, mà bạn sẽ bất ngờ, hãy cùng Điện thoại Giá Kho tìm hiểu ngay!'
+    },
+    {
+        id: 8,
+        name: 'Macbook air 2020',
+        price: 1200,
+        url: require('../../assets/images/apple-macbook-air-2020.jpg'),
+        isFavorite: false,
+        categoryId: 2,
+        description: 'Trong bộ 3 iPhone 11 Series, iPhone 11 64GB tuy là phiên bản thường nhưng được hội tụ nhiều yếu tố hấp dẫn nhất, với thiết kế trẻ trung, sở hữu nhiều phiên bản màu nhất, kích thước vừa phải, không quá to và không quá nhỏ. Và còn một điều nữa, mà bạn sẽ bất ngờ, hãy cùng Điện thoại Giá Kho tìm hiểu ngay!'
+    },
+    {
+        id: 9,
+        name: 'Airpods 2',
+        price: 200,
+        url: require('../../assets/images/bluetooth-airpods-2-apple.jpg'),
+        isFavorite: false,
+        categoryId: 5,
+        description: 'Trong bộ 3 iPhone 11 Series, iPhone 11 64GB tuy là phiên bản thường nhưng được hội tụ nhiều yếu tố hấp dẫn nhất, với thiết kế trẻ trung, sở hữu nhiều phiên bản màu nhất, kích thước vừa phải, không quá to và không quá nhỏ. Và còn một điều nữa, mà bạn sẽ bất ngờ, hãy cùng Điện thoại Giá Kho tìm hiểu ngay!'
+    },
+    {
+        id: 10,
+        name: 'Airpods Pro 2',
+        price: 300,
+        url: require('../../assets/images/tai-nghe-bluetooth-airpods-pro-2.jpeg'),
+        isFavorite: true,
+        categoryId: 5,
         description: 'Trong bộ 3 iPhone 11 Series, iPhone 11 64GB tuy là phiên bản thường nhưng được hội tụ nhiều yếu tố hấp dẫn nhất, với thiết kế trẻ trung, sở hữu nhiều phiên bản màu nhất, kích thước vừa phải, không quá to và không quá nhỏ. Và còn một điều nữa, mà bạn sẽ bất ngờ, hãy cùng Điện thoại Giá Kho tìm hiểu ngay!'
     }
+    
 ]
 
 export const LIST_PRODUCT_IN_CART = [
